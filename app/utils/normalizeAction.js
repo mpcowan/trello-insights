9b0ce891d2f4e500a6174e4b4095fb58606a895e
Matt Cowan <mcowan@atlassian.com>
2020-10-12 09:34:46 -0400

    Add eslint

--- app/utils/normalizeAction.js
+++ app/utils/normalizeAction.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 
-export default function (action) {
+export default (action) => {
   if (['updateBoard', 'updateCard', 'updateList', 'updateChecklist'].includes(action.type)) {
     const updatedKeys = _.keys(action.data.old);
     return `${action.type}:${updatedKeys[0]}`;
   }
   return action.type;
-}
+};
